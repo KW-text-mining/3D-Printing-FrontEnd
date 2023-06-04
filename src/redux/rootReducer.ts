baa89f836a6c459b61d57d2328c19ecddbb7cfe8
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-06-04 20:49:20 +0900

    [Fix] : remove unused file

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,15 +1,9 @@
 import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 // slices
 import wordcloudReducer from './slices/wordcloudbutton'
 import startendReducer from './slices/startenddate';
 import bigcateReducer from './slices/bigcatebutton'
-import mailReducer from './slices/mail';
-import chatReducer from './slices/chat';
-import productReducer from './slices/product';
-import calendarReducer from './slices/calendar';
-import kanbanReducer from './slices/kanban';
 
 // ----------------------------------------------------------------------
 
@@ -31,11 +25,6 @@ const rootReducer = combineReducers({
   wordcloudbutton : wordcloudReducer,
   startenddate : startendReducer,
   bigcatebutton : bigcateReducer,
-  mail: mailReducer,
-  chat: chatReducer,
-  calendar: calendarReducer,
-  kanban: kanbanReducer,
-  product: persistReducer(productPersistConfig, productReducer),
 });
 export type RootState = ReturnType<typeof rootReducer>;
 export default rootReducer;
