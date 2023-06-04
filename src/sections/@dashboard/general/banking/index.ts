639aa93932df8f5f82dbbcfe4e806c69bedf2846
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-06-04 21:04:12 +0900

    [Fix] : remove unused file

--- src/sections/@dashboard/general/banking/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as BankingContacts } from './BankingContacts';
-export { default as BankingQuickTransfer } from './BankingQuickTransfer';
-export { default as BankingInviteFriends } from './BankingInviteFriends';
-export { default as BankingWidgetSummary } from './BankingWidgetSummary';
-export { default as BankingCurrentBalance } from './BankingCurrentBalance';
-export { default as TotalAnalytics } from './TotalAnalytics';
-export { default as BankingRecentTransitions } from './BankingRecentTransitions';
-export { default as BankingExpensesCategories } from './BankingExpensesCategories';
