bb4bacb8eee8be734c47af4915b719a26670cd81
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-06-10 21:21:06 +0900

    [Fix] : not HashRouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Navigate, useRoutes } from 'react-router-dom';
+import { HashRouter, Navigate, useRoutes } from 'react-router-dom';
 
 // layouts
 import DashboardLayout from '../layouts/dashboard';
@@ -12,22 +12,20 @@ import {
 // ----------------------------------------------------------------------
 
 export default function Router() {
-  return (
-    <HashRouter>
-      {useRoutes([
-        // Dashboard
-        {
-          path: 'dashboard',
-          element: <DashboardLayout />,
-          children: [
-            { element: <Navigate to="/dashboard/app" replace />, index: true },
-            { path: 'app', element: <GeneralAppPage /> },
-            { path: 'analytics', element: <GeneralAnalyticsPage /> },
-          ],
-        },
-        // Main Page
-        { path: '*', element: <Navigate to="/dashboard/app" replace /> },
-      ])}
-    </HashRouter>
-  );
+  return useRoutes([
+    // Dashboard
+    {
+      path: 'dashboard',
+      element: (
+          <DashboardLayout />
+      ),
+      children: [
+        { element: <Navigate to='/dashboard/app' replace />, index: true },
+        { path: 'app', element: <GeneralAppPage /> },
+        { path: 'analytics', element: <GeneralAnalyticsPage /> },
+      ],
+    },
+    // Main Page
+    { path: '*', element: <Navigate to="/dashboard/app" replace /> },
+  ]);
 }
