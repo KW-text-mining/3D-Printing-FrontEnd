ae4debd44695632792885f4d9e76229dac3cbf14
heajinkwon <67617475+heajinkwon@users.noreply.github.com>
2023-06-11 20:32:32 +0900

    [Fix] : Complete Build&Deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -11,9 +10,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(<HashRouter>
-    <App />
-  </HashRouter>);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
